43324f34eebc8feadca26dbe9f803c4dfc6cac3f
pro_c <hieutg244@gmail.com>
2025-05-14 22:45:02 +0700

    update

--- Assets/NextSol/Game Package/Scripts/GameUIManager.cs
+++ Assets/NextSol/Game Package/Scripts/GameUIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
+using Jackal;
 using UnityEngine.EventSystems;
 
 public class GameUIManager : MonoBehaviour
